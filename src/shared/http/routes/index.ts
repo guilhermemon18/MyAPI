5a95615d984fa422314f9b51daf83ccc89e86b45
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-09-01 11:24:48 -0300

    Add roles routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,13 +1,14 @@
-import { AppError } from '@shared/errors/AppError';
 import { Router } from 'express';
+import { rolesRouter } from '@roles/http/routes/roles.routes';
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
-  throw new AppError('Acesso negado');
   return response.status(200).json({
     message: 'Olá Dev!',
   });
 });
+
+routes.use('/roles', rolesRouter);
 
 export { routes };
