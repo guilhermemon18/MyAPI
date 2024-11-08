6ba485e484198d772d25877e00e51c28723f148b
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-11-08 03:11:16 -0300

    Create usecase showrole

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,6 +1,7 @@
 import { RolesRepository } from '@roles/repositories/RolesRepository';
 import { createRolesController } from '@roles/useCases/createRole';
 import { listRolesController } from '@roles/useCases/listRoles';
+import { showRolesController } from '@roles/useCases/showRole';
 import { Router } from 'express';
 
 const rolesRouter = Router();
@@ -11,6 +12,10 @@ rolesRouter.post('/', (request, response) => {
 
 rolesRouter.get('/', (request, response) => {
   return listRolesController.handle(request, response); //status = 200 é padrao de 'sucess'
+});
+
+rolesRouter.get('/:id', (request, response) => {
+  return showRolesController.handle(request, response);
 });
 
 export { rolesRouter };
