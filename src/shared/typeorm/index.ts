792094ac0ca7559b76496c77cebfe62a7208ed53
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-11-09 03:58:49 -0300

    Create migration AddRoleIdToUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,10 +2,15 @@ import { Role } from '@roles/entities/Role';
 import { DataSource } from 'typeorm';
 import { CreateRolesTable1730932314556 } from './migrations/1730932314556-CreateRolesTable';
 import { CreateUsersTable1657996796706 } from './migrations/1731132644694-CreateUsersTable';
+import { AddRoleIdToUsersTable1731134747477 } from './migrations/1731134747477-AddRoleIdToUsersTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1730932314556, CreateUsersTable1657996796706],
+  migrations: [
+    CreateRolesTable1730932314556,
+    CreateUsersTable1657996796706,
+    AddRoleIdToUsersTable1731134747477,
+  ],
 });
