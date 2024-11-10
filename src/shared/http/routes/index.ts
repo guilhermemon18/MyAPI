c82c7cd33fd41b925fa6b4085c5d3a9d4fcd1195
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-11-10 06:07:43 -0300

    Add CreateUser route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,6 @@
-import { Router } from 'express';
 import { rolesRouter } from '@roles/http/routes/roles.routes';
+import { usersRouter } from '@users/http/user.routes';
+import { Router } from 'express';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.get('/', (request, response) => {
 });
 
 routes.use('/roles', rolesRouter);
+routes.use('/users', usersRouter);
 
 export { routes };
