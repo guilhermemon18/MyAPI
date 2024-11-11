785f924a90cbb99e8f227c60bad50dfa7a153a94
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-11-11 03:25:21 -0300

    Create seed admin user

---
+++ src/shared/typeorm/seeds/admin.ts
@@ -0,0 +1,23 @@
+import { dataSource } from '@shared/typeorm';
+import { hash } from 'bcryptjs';
+import { v4 as uuidV4 } from 'uuid';
+async function create() {
+  const connection = await dataSource.initialize();
+  // Create Role
+  const roleId = uuidV4();
+  await connection.query(
+    `INSERT INTO ROLES(id, name)
+      values('${roleId}', 'T.I.')
+    `,
+  );
+  // Create User
+  const userId = uuidV4();
+  const password = await hash('1234', 10);
+  await connection.query(
+    `INSERT INTO USERS(id, name, email, password, isAdmin, roleId)
+      values('${userId}', 'admin', 'a@a.com', '${password}', true, '${roleId}')
+    `,
+  );
+  await connection.destroy();
+}
+create().then(() => console.log('User admin created!'));
